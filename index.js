a70ad76d398205d780f23c53ca07f71613d578f9
Coleman Moore <morecoleman@gmail.com>
2021-09-28 15:43:35 -0700

    analyzing number of answers for randomly chosen pangrams

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const fs = require('fs')
+const _ = require('underscore')
 
-const words_json = fs.readFileSync('words_dictionary.json')
-const words = JSON.parse(words_json)
-const pangrams = Object.keys(words).filter(canBePangram)
+const words_json = fs.readFileSync('dictionary.json')
+const wordsHash = JSON.parse(words_json)
+const wordsList = Object.keys(wordsHash)
+
+const pangrams = wordsList.filter(canBePangram)
 
 const chosenPangram = choosePangram()
-let letters = uniqueChars(chosenPangram)
-
-letters = shuffleChars(letters)
+const letters = uniqueChars(chosenPangram)
 
 console.log(`Letters: ${letters}`)
 console.log(`Pangram: ${chosenPangram}`)
 
-
+letters.forEach(centerLetter => {
+    const answers = getQualifyingWords(centerLetter)
+    console.log(`${centerLetter}: ${answers.length}`)
+    console.log(answers.slice(0,10))
+})
 
 
 function canBePangram(word) {
@@ -29,7 +34,7 @@ function uniqueChars(word) {
     const chars = word.split('')
     const uniques = chars.filter((value, index, self) =>
         self.indexOf(value) === index)
-    return uniques
+    return shuffleChars(uniques)
 }
 
 function shuffleChars(chars) {
@@ -41,4 +46,20 @@ function shuffleChars(chars) {
         array[j] = temp
     }
     return array
+}
+
+function getQualifyingWords(centerLetter) {
+    let answers = []
+
+    wordsList.forEach(word => {
+
+        const uniques = uniqueChars(word)
+        if (!_.contains(uniques, centerLetter)) return
+
+        const wrong = _.without(uniques, ...letters)
+        if (wrong.length > 0) return
+
+        answers.push(word)
+    })
+    return answers
 }
