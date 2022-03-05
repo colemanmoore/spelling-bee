b42d74ddf63f2ab5fcdbf47d59d014914bd89cc7
Coleman Moore <morecoleman@gmail.com>
2022-03-04 20:36:44 -0600

    using pg

--- bin/generate.mjs
+++ bin/generate.mjs
@@ -8,19 +8,18 @@ const game = await Game.createNewGame(7)
 console.log(`End finding pangram. (${(new Date()).toLocaleTimeString()})\n${game.toString()}`)
 
 try {
-    // await game.save()
-    await updateGame(63, game)
+    await game.save()
 
 } catch (e) {
     console.log(e)
     console.log('Error saving game')
 }
-/*
+
 try {
     await clearOldestGame()
 } catch (e) {
     console.log(e)
     console.log('Error clearing old game')
 }
-*/
+
 process.exit(0)
